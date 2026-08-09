b0aade04119ddbea22c1957e715f1c1da4f21859
Saman <saman.bahrekazemi@gmail.com>
2026-08-09 20:11:13 +0330

    Add backend api configs.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   devIndicators: false,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 export default nextConfig;
